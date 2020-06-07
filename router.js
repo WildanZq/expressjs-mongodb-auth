bbc683aef19a0042597a251f7d49de5c57dc061e
Wildan Ziaulhaq <wildanzhq@gmail.com>
2020-06-07 22:23:32 +0700

    signin and jwt

--- router.js
+++ router.js
@@ -1,5 +1,14 @@
 const Auth = require('./controllers/auth');
+const passportService = require('./services/passport');
+const passport = require('passport');
+
+const requireAuth = passport.authenticate('jwt', { session: false });
+const requireSignIn = passport.authenticate('local', { session: false });
 
 module.exports = function(app) {
+  app.get('/', requireAuth, function(req, res) {
+    res.send({ hello: 'world' });
+  });
+  app.post('/signin', requireSignIn, Auth.signin);
   app.post('/signup', Auth.signup);
 }
