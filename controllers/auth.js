3febf7a76f0bb4dc1adb4aca2e5ea91b9de1a1ae
Wildan Ziaulhaq <wildanzhq@gmail.com>
2020-06-07 19:39:38 +0700

    handle signup

---
+++ controllers/auth.js
@@ -0,0 +1,29 @@
+const User = require('../models/user');
+
+exports.signup = function(req, res, next) {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  if (!email || !password) {
+    return res.status(422).send({ error: 'You must provide the required data' });
+  }
+
+  User.findOne({ email: email }, function(err, existingUser) {
+    if (err) return next(err);
+
+    if (existingUser) {
+      return res.status(422).send({ error: 'Email is already used' });
+    }
+
+    const user = new User({
+      email: email,
+      password: password
+    });
+
+    user.save(function(err) {
+      if (err) return next(err);
+
+      res.json({ success: true });
+    });
+  });
+}
